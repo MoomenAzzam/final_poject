bd70d95f373d44d60a34a01779169cfecc5e0e12
cs310 <cs310.se@gmail.com>
2021-12-16 21:01:01 +0200

    تعديل على التصميم

--- app/src/main/java/com/example/finalproject/fragment/homeFragment/FavoriteFragment.kt
+++ app/src/main/java/com/example/finalproject/fragment/homeFragment/FavoriteFragment.kt
@@ -14,7 +14,7 @@ class FavoriteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite2, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
 }
