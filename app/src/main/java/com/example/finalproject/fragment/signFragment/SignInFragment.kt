1a39958f50667ced70baac791c71ff2d99d4342c
cs310 <cs310.se@gmail.com>
2021-12-21 19:12:20 +0200

    create a fragment for add book update a fragment for the description
    don't focus on everything

--- app/src/main/java/com/example/finalproject/fragment/signFragment/SignInFragment.kt
+++ app/src/main/java/com/example/finalproject/fragment/signFragment/SignInFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.finalproject.R
 
-class LoginFragment : Fragment() {
+class SignInFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
 
