7ecaed253e417a6281f223ebef1dfafe533a0f91
bilal0499 <BilalOsama.2002.6.17@gmail.com>
2021-12-21 22:45:24 +0200

    Signup screen design and programming

--- app/src/main/java/com/example/finalproject/fragment/signFragment/SignUpFragment.kt
+++ app/src/main/java/com/example/finalproject/fragment/signFragment/SignUpFragment.kt
@@ -1,21 +1,62 @@
 package com.example.finalproject.fragment.signFragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.finalproject.R
+import com.example.finalproject.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
-
+     val binding = FragmentSignUpBinding.inflate(layoutInflater)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val singUpView = inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        //The button that when pressed saves the information of the new user
+        if(binding.tvName.text.isNotEmpty() && binding.tvPassword.text.isNotEmpty()
+            && binding.tvRePassword.text.isNotEmpty()){
+            binding.btnSignUp.setOnClickListener {
+                val username = binding.tvName.text.toString()
+                val email = binding.tvEmail.text.toString()
+                val pass = binding.tvPassword.text.toString()
+                val RePass = binding.tvPassword.text.toString()
+
+                //To check whether the password in the first field is the same as in the second field
+                if(pass.equals(RePass) ){
+                    val bundle = Bundle()
+                    bundle.putString("nameSignUp",username)
+                    bundle.putString("emailSignUp",email)
+                    bundle.putString("PasswordSignUp",pass)
+                    bundle.putString("RePasswordSignUp",RePass)
+                    /*
+                    Here you enter the location to which you will transfer this data
+                    */
+                    swapFragment(SignInFragment())
+                }else{
+                    Toast.makeText(context , "Please verify that the password is correct" , Toast.LENGTH_SHORT).show()
+                }
+            }
+        }else
+            Toast.makeText(context , "Please Fill in The Required Fields" , Toast.LENGTH_SHORT).show()
+//******************************************************  signInLink  *********************************************
+        // The text that the user will click on if they already have an account
+        binding.singInLink.setOnClickListener {
+            swapFragment(SignInFragment())
+        }
+        return singUpView
+    }
+
+    @SuppressLint("UseRequireInsteadOfGet")
+    fun swapFragment(fragment: Fragment){
+        activity!!.supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,fragment)
+            .addToBackStack(null).commit()
     }
 
 
