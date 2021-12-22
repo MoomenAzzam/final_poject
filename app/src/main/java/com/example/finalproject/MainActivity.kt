13423a666e01f134db650dae6818b54b0c3bfbc3
cs310 <cs310.se@gmail.com>
2021-12-22 11:31:44 +0200

    update a fragment for the profile
    don't focus on everything

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        swipeFragment(this, MainFragment())
+//        swipeFragment(this, MainFragment())
 
     }
 
