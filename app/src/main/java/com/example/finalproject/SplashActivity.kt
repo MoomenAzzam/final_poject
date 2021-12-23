e33866f9818b09955bc2ba9e986db3ce9a672877
cs310 <cs310.se@gmail.com>
2021-12-23 06:01:10 +0200

    create activity_splash.xml and add the code in it
    don't focus on everything

---
+++ app/src/main/java/com/example/finalproject/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.finalproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        hide status par
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+//            hide action par
+//        supportActionBar?.hide()
+        Handler().postDelayed({        val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            finish()},3000)
+    }
+}
